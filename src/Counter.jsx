59e10adc01598e21420262fee916ab02cd2245b3
Juan Hincapie <juegojuank@gmail.com>
2025-10-06 21:39:25 -0500

    fix: add limit and max to counter component

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -2,11 +2,29 @@ import { useState } from "react";
 
 const Counter = () => {
     const [count, setCount] = useState(0);
+    const LIMIT_MAX = 10;
 
     return (
         <div>
             <h1>Counter: <span>{count}</span></h1>
-            <button onClick={ () => setCount(count + 1)}>Increment</button>
+            <button onClick={() => {
+                if (count < LIMIT_MAX) {
+                    setCount(count + 1);
+                }
+            }}
+            disabled={count === LIMIT_MAX}
+            >Increment</button>
+
+            <button onClick={() => {
+                if (count > 0) {
+                    setCount(count - 1);
+                }
+            }}
+            disabled={count === 0}
+            >Decrement</button>
+            
+            {count === LIMIT_MAX && <p>¡Has llegado al máximo!</p>}
+            {count === 0 && <p>No puedes bajar de cero.</p>}
         </div>
     );
 }
