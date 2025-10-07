6f0913eb8778af0e0d4d2391b1eb086ee7803f9e
Juan Hincapie <juegojuank@gmail.com>
2025-10-06 21:07:32 -0500

    feat: add component counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import ButtonPrimary from './ButtonPrimary.jsx'
+import Counter from './counter.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+
       <ButtonPrimary url="https://github.com/juankh993/estudiandoReact/commit/dbb50c0724795e1ded58569781d0c4704a06cadb" text="Botón con destructuring"/>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <Counter />
     </>
   )
 }
