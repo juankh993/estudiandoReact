059693b28d4ac6bb66740b223d2933917be00e41
Juan Hincapie <juegojuank@gmail.com>
2025-10-07 21:35:51 -0500

    feat: add hook useEfecct a component counter

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,30 +1,42 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Counter = () => {
     const [count, setCount] = useState(0);
     const LIMIT_MAX = 10;
 
+    useEffect(() => {
+        document.title = `Counter: ${count}`;
+        console.log("El contador ha cambiado:", count);
+    }, [count]);
+
     return (
         <div>
             <h1>Counter: <span>{count}</span></h1>
-            <button onClick={() => {
-                if (count < LIMIT_MAX) {
-                    setCount(count + 1);
-                }
-            }}
-            disabled={count === LIMIT_MAX}
-            >Increment</button>
+            <button
+                onClick={() => {
+                    if (count < LIMIT_MAX) {
+                        setCount(count + 1);
+                    }
+                }}
+                disabled={count === LIMIT_MAX}
+            >
+                Increment
+            </button>
 
-            <button onClick={() => {
-                if (count > 0) {
-                    setCount(count - 1);
-                }
-            }}
-            disabled={count === 0}
-            >Decrement</button>
-            
+            <button
+                onClick={() => {
+                    if (count > 0) {
+                        setCount(count - 1);
+                    }
+                }}
+                disabled={count === 0}
+            >
+                Decrement
+            </button>
+
             {count === LIMIT_MAX && <p>¡Has llegado al máximo!</p>}
             {count === 0 && <p>No puedes bajar de cero.</p>}
+
         </div>
     );
 }
